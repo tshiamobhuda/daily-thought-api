63966c7d2d1df18d3ad1ef4308c82d4926214ee2
Tshiamo <tshiamobhuda@gmail.com>
2020-08-18 01:11:58 +0200

    Use axios & cheerio to visit & scrap site - use regex to pull the verse, notes & prayer

--- functions/set-daily-thought.js
+++ functions/set-daily-thought.js
@@ -1,6 +1,25 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
 exports.handler = function (event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: 'Should not return anything'
-    })
+    axios.get('https://alivetogod.com/daily-thoughts')
+    .then(function (response) {
+        if (response.status === 200) {
+            const $ = cheerio.load(response.data);
+            const $content = $('.daily-thought').find('.more-text').first();
+
+            const verse = $content.find('strong').first().text().match(/(?!.*verse|:)\b((?!‘|\.).)+/gi);
+            const notes = $content.text().trim().match(/-\s\b((?!\.|(prayer)).)+/gi);
+            const prayer = $content.text().trim().match(/(?!.*prayer|:)\b(.)+/gi)
+
+            callback(null, {
+                statusCode: 200,
+                body: 'Ok'
+            })
+        }
+    }).catch(function (err) {
+        console.log('Axios | Error occurred', err)
+        
+        callback(err);
+    });
 }
